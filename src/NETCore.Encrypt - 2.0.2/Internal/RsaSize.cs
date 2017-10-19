22d92894ada567a3447beb5ba4f41ef9595d797a
Catcher Wong <catcher_hwq@outlook.com>
2017-10-19 20:49:16 +0800

    test (#2)
    * Update README.md
    
    * 更新版本到2.0.3

---
+++ src/NETCore.Encrypt - 2.0.2/Internal/RsaSize.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NETCore.Encrypt
+{
+    public enum RsaSize
+    {
+        R2048=2048,
+        R3072=3072,
+        R4096=4096
+    }
+}
